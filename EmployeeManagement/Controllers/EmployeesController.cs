a806615701f329e6657a2cde9bc22d1b8577be05
madushak <madushak@embla.asia>
2023-08-09 21:39:19 +0530

    change api end points

--- EmployeeManagement/Controllers/EmployeesController.cs
+++ EmployeeManagement/Controllers/EmployeesController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace EmployeeManagement.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("Employees")]
     public class EmployeeController : ControllerBase
     {
         private readonly IEmployeeService _employeeService;
